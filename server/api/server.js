ad2fffd989b4e64d9c92720eff6dd8d7277ec332
sansetocapital <sansetocapital@gmail.com>
2024-12-13 14:06:02 +0200

    Update on functions

--- server/api/server.js
+++ server/api/server.js
@@ -6,6 +6,8 @@ const authRoute = require('./routes/authRoute');
 const adminRoute = require('./routes/adminRoute');
 require('dotenv').config();
 const { createAdmin } = require('./controllers/createAdmin');
+const { subScribe } = require('./controllers/authController');
+const {getAllSubscribers, getGroupedSubscriptions, manageSubscription, loginAmin} = require('./controllers/adminController');
 const subscriptionService = require("./services/subscriptionService");
 const { MONGO_URI } = process.env;
 
@@ -17,8 +19,10 @@ app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 
 // Routes
-app.use('/api', authRoute);
-app.use('/api/admin', adminRoute);
+app.use('/api/user', subScribe);
+app.use('/api/admin/login', loginAdmin);
+router.get('/api/admin/getAllSubscription', getGroupedSubscriptions)
+router.post('/api/admin/manageSubscription', manageSubscription);
 
 // MongoDB Connection
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
