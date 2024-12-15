db72c853f5c2da5d4cdab8adfe3485fd134f73d7
vitalii0801 <stadnykvitalii81@gmail.com>
2024-12-16 00:45:17 +0200

    Update on server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import PublicForm from './pages/publicForm';
-import AdminPanel from './pages/adminPanel';
+import UserTable from './pages/userTable'
 import Index from './pages/index';
 import LoginPage from './pages/login';
+import SubscriptionTable from './pages/subscriptionTable';
 
 function App() {
   const ProtectedRoute = ({children}) => {
@@ -17,8 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Index></Index>} />
         <Route path="/user/subscription" element = {<PublicForm role="user"/>} />
-        <Route path="/admin" element={<ProtectedRoute><AdminPanel /></ProtectedRoute>} />
         <Route path="/admin/subscription" element={<PublicForm role="admin"/>} />
+        <Route path="/admin/table/users" element={<ProtectedRoute><UserTable/></ProtectedRoute>} />
+        <Route path="/admin/table/subscriptions" element={<ProtectedRoute><SubscriptionTable role="admin"/></ProtectedRoute>} />
         <Route path="/admin/login" element={<LoginPage />} />
       </Routes>
     </Router>
